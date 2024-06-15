780a8162a0ceb3a13597ac7184efbcce6738b8ed
Charis02 <chge2002@gmail.com>
2024-06-14 22:59:58 -0700

    fix site og issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://charis02.github.io",
+    site: "http://charis02.github.io/cyprus_webpage/",
     base: "cyprus_webpage",
     integrations: [tailwind()],
   });
